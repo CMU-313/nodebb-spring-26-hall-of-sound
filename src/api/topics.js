4798ed9b68e448cec4930c20e2d4fd246021fde7
Nikash B <nikashbhardwaj@gmail.com>
2026-01-30 21:28:43 +0000

    Added question and note tags to topic posts.

--- src/api/topics.js
+++ src/api/topics.js
@@ -57,6 +57,9 @@ topicsAPI.create = async function (caller, data) {
 	const payload = { ...data };
 	delete payload.tid;
 	payload.tags = payload.tags || [];
+	if (payload.topicType) {
+		payload.tags.push(payload.topicType === 'question' ? 'Question' : 'Note');
+	}
 	apiHelpers.setDefaultPostData(caller, payload);
 	const isScheduling = parseInt(data.timestamp, 10) > payload.timestamp;
 	if (isScheduling) {
